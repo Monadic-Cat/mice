91d7fb441e88842247cdb025fb16edd36daf33dc
Monadic Cat <greenmonadcat@gmail.com>
2019-08-09 10:38:45 -0500

    Add a benchmark

---
+++ benches/my_benchmark.rs
@@ -0,0 +1,11 @@
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
+
+fn rolling_benchmark(c: &mut Criterion) {
+    c.bench_function("rolls", |b| {
+        b.iter(|| mice::roll(black_box("100000d100")))
+    });
+}
+
+
+criterion_group!(benches, rolling_benchmark);
+criterion_main!(benches);
