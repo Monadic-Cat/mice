2598d09404d0c47f24fb02144723ec2cb1f5b8bf
Monadic Cat <greenmonadcat@gmail.com>
2019-08-12 08:21:02 -0500

    Format benchmark

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,11 +1,8 @@
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 fn rolling_benchmark(c: &mut Criterion) {
-    c.bench_function("rolls", |b| {
-        b.iter(|| mice::roll(black_box("100000d100")))
-    });
+    c.bench_function("rolls", |b| b.iter(|| mice::roll(black_box("100000d100"))));
 }
-
 
 criterion_group!(benches, rolling_benchmark);
 criterion_main!(benches);
